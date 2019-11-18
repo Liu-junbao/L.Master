f10e3d110d5e623277be0862fc99e6c742225719
Administrator <Administrator@DESKTOP-E1PQILQ>
2019-11-18 17:21:25 +0800

    123456

--- L.Wpf.Toolkit/CustomButton.cs
+++ L.Wpf.Toolkit/CustomButton.cs
@@ -9,9 +9,17 @@ namespace System.Windows
 {
     public class CustomButton : Button
     {
+        public static readonly DependencyProperty PressedBackgroundProperty =
+          DependencyProperty.Register(nameof(PressedBackground), typeof(Brush), typeof(CustomButton), new PropertyMetadata(default(Brush)));
         static CustomButton()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomButton), new FrameworkPropertyMetadata(typeof(CustomButton)));
+        }
+        [Category("Appearance")]
+        public Brush PressedBackground
+        {
+            get { return (Brush)GetValue(PressedBackgroundProperty); }
+            set { SetValue(PressedBackgroundProperty, value); }
         }
     }
 }
