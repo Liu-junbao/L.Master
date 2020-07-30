0fa83d08a3a4f3c72f618b78552d4364fc548078
Administrator <Administrator@DESKTOP-E1PQILQ>
2020-07-30 10:38:59 +0800

    123456

--- L.Wpf.Toolkit/PupBox.cs
+++ L.Wpf.Toolkit/PupBox.cs
@@ -9,26 +9,16 @@ using System.Windows.Media;
 
 namespace System.Windows
 {
-    [ContentProperty(nameof(Header))]
     public class PupBox : ItemsControl
     {
         public static readonly DependencyProperty HeaderProperty =
             DependencyProperty.Register(nameof(Header), typeof(object), typeof(PupBox), new PropertyMetadata(null));
         public static readonly DependencyProperty PlacementProperty =
             DependencyProperty.Register(nameof(Placement), typeof(PlacementMode), typeof(PupBox), new PropertyMetadata(PlacementMode.Bottom));
-        public static readonly DependencyProperty IsDropDownOpenProperty =
-            DependencyProperty.Register(nameof(IsDropDownOpen), typeof(bool), typeof(PupBox), new PropertyMetadata(false));
         static PupBox()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(PupBox), new FrameworkPropertyMetadata(typeof(PupBox)));
         }
-        public PupBox()
-        {
-            EventManager.RegisterClassHandler(typeof(PupBox),Mouse.MouseUpEvent,new RoutedEventHandler(OnMouseUpHandler));
-            EventManager.RegisterClassHandler(typeof(PupBox),ButtonBase.ClickEvent, new RoutedEventHandler(OnClickHandler));
-        }
-
-     
         public object Header
         {
             get { return (object)GetValue(HeaderProperty); }
@@ -39,31 +29,6 @@ namespace System.Windows
             get { return (PlacementMode)GetValue(PlacementProperty); }
             set { SetValue(PlacementProperty, value); }
         }
-        public bool IsDropDownOpen
-        {
-            get { return (bool)GetValue(IsDropDownOpenProperty); }
-            set { SetValue(IsDropDownOpenProperty, value); }
-        }
-        protected override void OnMouseLeave(MouseEventArgs e)
-        {
-            base.OnMouseLeave(e);
-            IsDropDownOpen = false;
-        }
-        protected override void OnMouseMove(MouseEventArgs e)
-        {
-            base.OnMouseMove(e);
-            IsDropDownOpen = true;
-        }
-        private void OnMouseUpHandler(object sender, RoutedEventArgs e)
-        {
-            if (IsMouseOver == false)
-                IsDropDownOpen = false;
-        }
-        private void OnClickHandler(object sender, RoutedEventArgs e)
-        {
-            IsDropDownOpen = false;
-        }
-
     }
 
     public static class PupBoxAssist
